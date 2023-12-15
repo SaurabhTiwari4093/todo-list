52be5d61ade26b92bf6f8c157278f696a432a811
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2023-12-15 06:03:07 +0530

    Updated home url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Router>
       <Header/>
       <Routes>
-        <Route path="/todo-list" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
       </Routes>
       <Footer/>
--- src/components/header/component.js
+++ src/components/header/component.js
@@ -7,14 +7,14 @@ export default function header() {
     return (
         <div><nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/todo-list">TODO-LIST</Link>
+          <Link className="navbar-brand" to="/">TODO-LIST</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active"  to="/todo-list">Home</Link>
+                <Link className="nav-link active"  to="/">Home</Link>
               </li>
              <li className="nav-item">
                 <Link className="nav-link active" to="/about">About</Link>
